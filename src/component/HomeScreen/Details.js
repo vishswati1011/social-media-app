1dc04218a6bbccae0894260f141be843916b1d7b
vishswat11011 <sati.v1011@gmail.com>
2022-11-29 18:29:15 +0530

    event modal

--- src/component/HomeScreen/Details.js
+++ src/component/HomeScreen/Details.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React ,{useState}from "react";
+
 const Details = () => {
+
+    const [show,setShow]=useState(false)
+    
+  
     return (
             <div class="card " style={{ width: "18rem" }} className="text-left">
                 <ul class="list-group">
-                    <li class="list-group-item">Cras justo odio</li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
-                    <li class="list-group-item">Vestibulum at eros</li>
+                    <li class="list-group-item">Linkedin News</li>
+                    <li class="list-group-item">Which sector are hiring in India</li>
+                    <li class="list-group-item">Fresher hiring on upswing</li>
                 </ul>
 
                 <div class="card-header" id="headingThree">
@@ -13,15 +18,15 @@ const Details = () => {
                         <div class="card-body">
                             <ul class="list-group">
 
-                                <li class="list-group-item">Cras justo odio</li>
-                                <li class="list-group-item">Dapibus ac facilisis in</li>
+                                <li class="list-group-item">New degignation for study abroad</li>
+                                <li class="list-group-item">SaaS players remain upbeat</li>
                                 <li class="list-group-item">Vestibulum at eros</li>
                             </ul>
                         </div>
                     </div>
                     <ul class="list-group">
-                        <li class="list-group-item" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            Show more
+                        <li class="list-group-item" data-toggle="collapse" onClick={()=>setShow(!show)}data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            {!show?"show more " :"show less"}
                         </li>
                     </ul>
                 </div>
--- src/component/UploderIcon/EventUploder.js
+++ src/component/UploderIcon/EventUploder.js
@@ -10,10 +10,10 @@ const FileUploader = () => {
     };
     return (
         <>
-           
-                <FcCalendar data-toggle="modal" data-target="#exampleModal"  onClick={handleClick} /> Event
-                <input type="file" style={{ display: 'none' }} ref={hiddenFileInput} />
-          
+
+            <FcCalendar data-toggle="modal" data-target="#exampleModal" onClick={handleClick} /> Event
+            <input type="file" style={{ display: 'none' }} ref={hiddenFileInput} />
+
             <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
@@ -26,20 +26,39 @@ const FileUploader = () => {
                         <div class="modal-body text-left">
                             <div className=''></div>
                             <div className='mb-3'>Event Type  </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input active" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
-                                        <label class="form-check-label" for="inlineRadio1">Online</label>
-                                </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
-                                        <label class="form-check-label" for="inlineRadio2">In Person</label>
-                                </div>
-                          
-                            <div class="form-inline">
-                                    <input class="form-input" type="text"  value="option2"/>
-                                        
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input active" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                                <label class="form-check-label" for="inlineRadio1">Online</label>
                             </div>
-                      
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                                <label class="form-check-label" for="inlineRadio2">In Person</label>
+                            </div>
+
+                            <form>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Event Name *</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Timezone </label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                                </div>
+                                <div class="form-row">
+                                <div class="form-group col-md-6">
+                                <label for="exampleInputPassword1">Start Date </label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                                    </div>
+                                <div class="form-group col-md-6">
+
+                                    <label for="exampleInputPassword1">End Date </label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                                </div>
+                                </div>
+                               
+                            </form>
+
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-outline-primary" style={{ borderRadius: "20px" }} data-dismiss="modal">Cancel</button>
